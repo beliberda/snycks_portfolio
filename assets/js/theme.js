8d46bae0e188b7638ce8638741934f2a885e95a3
beliberda <anton.nurgaliev@mail.ru>
2023-11-21 22:21:56 +1000

    wqdewq

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -21,3 +21,49 @@ light.onclick = function () {
 
 
 }
+
+
+
+function replaceTemplateAttribute(product, template) {
+    const attributeMass = [
+        {
+            title: "name",
+            text: "{название товара}"
+        },
+        {
+            title: "category",
+            text: "{категория товара}"
+        },
+        {
+            title: "size",
+            text: "{размер товара}"
+        },
+        {
+            title: "url",
+            text: "{ссылка на товар}"
+        },
+    ]
+
+    attributeMass.forEach(element => {
+        let temp
+        for (const key of Object.keys(product)) {
+            if (key === element.title) {
+                temp = product[key]
+                console.log("temp", temp);
+                break
+            }
+        }
+        template = template.replace(element.text, temp)
+    });
+    return template
+
+}
+
+product = {
+    name: "Джинсы",
+    size: "XXL",
+    category: "худи",
+    url: "https://youtube.com"
+}
+template = "{название товара} супер пупер {категория товара} размера {размер товара}, доступен по ссылке {ссылка на товар}"
+console.log(replaceTemplateAttribute(product, template));
