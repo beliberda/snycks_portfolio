28c363a892d1ad8617e3a99f9f7b1255fb1a8e7a
beliberda <anton.nurgaliev@mail.ru>
2023-11-25 20:02:58 +1000

    slider radio

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,1 +1,89 @@
-massSlides = [1, 2, 3, 4, 5, 6, 7, 8]
+massSlides = [
+  {
+    title: "Бабочка",
+    text: "Бабочка - это К.Р.У.Т.О",
+    link: "",
+    url: "./assets/images/portfolio/butter.jpg"
+  },
+  {
+    title: "Капибара",
+    text: "А может лучше КАРБЮРАТОР?",
+    link: "",
+    url: "./assets/images/portfolio/CAPIBARA.jpg"
+  },
+  {
+    title: "Это Джо Джо референс",
+    text: "Невероятные приключения ДжоДжо, часто сокращаемая до JoJo или JJBA, - это франшиза манги и аниме, жанра Сёнэн и Сэйнэн, которая повествует о приключениях членов семьи Джостаров в разных поколения",
+    link: "https://animego.org/anime/neveroyatnoe-priklyuchenie-dzhodzho-nesokrushimyy-almaz-988",
+    url: "./assets/images/portfolio/jojo.jpg"
+  },
+  {
+    title: "Зима близко",
+    text: "Winter is coming",
+    link: "",
+    url: "./assets/images/portfolio/winter.jpg"
+  },
+]
+
+let left = document.getElementById("left-slide")
+let right = document.getElementById("right-slide")
+let slider = document.querySelector(".slider")
+
+let pagination = document.querySelector(".slider-pagination")
+
+for (let i = 0; i < massSlides.length; i++) {
+  pagination.insertAdjacentHTML("beforeend", `<label class="radio-slide">
+            <input type="radio" name="slider" />
+            <span></span>
+          </label>`)
+}
+pagination = document.querySelectorAll(".radio-slide")
+
+console.log(pagination);
+
+pagination.forEach((radio, i) => {
+  radio.onclick = function () {
+    DrawSlide(i)
+  }
+});
+
+function DrawSlide(current) {
+  slider.innerHTML = `<div class="slider-slide">
+
+            <img
+              src="${massSlides[current].url}"
+              alt=""
+              class="slider__project-image"
+            />
+            <main class="slider-main">
+              <h2 class="slider-main__title">${massSlides[current].title}</h2>
+              <p class="slider-main__text">
+                ${massSlides[current].text}
+              </p>
+              <button class="btn-demo"><a target="_blank" href="${massSlides[current].link}">Demo</a></button>
+            </main>
+            
+          </div>`
+  pagination[current].firstElementChild.checked = true
+}
+
+current = 0
+DrawSlide(current)
+
+left.onclick = () => {
+  current--
+  if (current == -1) {
+    current = massSlides.length - 1
+  }
+  DrawSlide(current)
+
+}
+right.onclick = () => {
+
+  current++
+  if (current == massSlides.length) {
+    current = 0
+  }
+  DrawSlide(current)
+
+}
