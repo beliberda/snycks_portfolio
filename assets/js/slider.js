a316c5e9006c94759eeeaaf979e6d9587d91d501
beliberda <anton.nurgaliev@mail.ru>
2023-11-26 22:28:50 +1000

    fix slider

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -3,48 +3,50 @@ massSlides = [
     title: "Бабочка",
     text: "Бабочка - это К.Р.У.Т.О",
     link: "",
-    url: "./assets/images/portfolio/butter.jpg"
+    url: "./assets/images/portfolio/butter.jpg",
   },
   {
     title: "Капибара",
     text: "А может лучше КАРБЮРАТОР?",
     link: "",
-    url: "./assets/images/portfolio/CAPIBARA.jpg"
+    url: "./assets/images/portfolio/CAPIBARA.jpg",
   },
   {
     title: "Это Джо Джо референс",
     text: "Невероятные приключения ДжоДжо, часто сокращаемая до JoJo или JJBA, - это франшиза манги и аниме, жанра Сёнэн и Сэйнэн, которая повествует о приключениях членов семьи Джостаров в разных поколения",
     link: "https://animego.org/anime/neveroyatnoe-priklyuchenie-dzhodzho-nesokrushimyy-almaz-988",
-    url: "./assets/images/portfolio/jojo.jpg"
+    url: "./assets/images/portfolio/jojo.jpg",
   },
   {
     title: "Зима близко",
     text: "Winter is coming",
     link: "",
-    url: "./assets/images/portfolio/winter.jpg"
+    url: "./assets/images/portfolio/winter.jpg",
   },
-]
+];
 
-let left = document.getElementById("left-slide")
-let right = document.getElementById("right-slide")
-let slider = document.querySelector(".slider")
+let left = document.getElementById("left-slide");
+let right = document.getElementById("right-slide");
+let slider = document.querySelector(".slider");
 
-let pagination = document.querySelector(".slider-pagination")
+let pagination = document.querySelector(".slider-pagination");
 
 for (let i = 0; i < massSlides.length; i++) {
-  pagination.insertAdjacentHTML("beforeend", `<label class="radio-slide">
+  pagination.insertAdjacentHTML(
+    "beforeend",
+    `<label class="radio-slide">
             <input type="radio" name="slider" />
             <span></span>
-          </label>`)
+          </label>`
+  );
 }
-pagination = document.querySelectorAll(".radio-slide")
-
-console.log(pagination);
-
+pagination = document.querySelectorAll(".radio-slide");
+let current = 0;
 pagination.forEach((radio, i) => {
   radio.onclick = function () {
-    DrawSlide(i)
-  }
+    current = i;
+    DrawSlide(i);
+  };
 });
 
 function DrawSlide(current) {
@@ -63,27 +65,23 @@ function DrawSlide(current) {
               <button class="btn-demo"><a target="_blank" href="${massSlides[current].link}">Demo</a></button>
             </main>
             
-          </div>`
-  pagination[current].firstElementChild.checked = true
+          </div>`;
+  pagination[current].firstElementChild.checked = true;
 }
 
-current = 0
-DrawSlide(current)
+DrawSlide(current);
 
 left.onclick = () => {
-  current--
+  current--;
   if (current == -1) {
-    current = massSlides.length - 1
+    current = massSlides.length - 1;
   }
-  DrawSlide(current)
-
-}
+  DrawSlide(current);
+};
 right.onclick = () => {
-
-  current++
+  current++;
   if (current == massSlides.length) {
-    current = 0
+    current = 0;
   }
-  DrawSlide(current)
-
-}
+  DrawSlide(current);
+};
